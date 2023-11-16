93c7a0f666fcbf38837bcabd113c39f10d36aaca
Nilesh Sharan Chaubey <105011486+Nilesh-51@users.noreply.github.com>
2023-11-16 18:51:35 +0530

    Update client.c

--- halfduplex/client.c
+++ halfduplex/client.c
@@ -1,36 +1,32 @@
 #include "stdio.h"
 #include "stdlib.h"
 #include "string.h"
-//headers for socket and related functions
 #include <sys/types.h>
 #include <sys/socket.h>
-//for including structures which will store information needed
 #include <netinet/in.h>
 #include <unistd.h>
-//for gethostbyname
 #include "netdb.h"
 #include "arpa/inet.h"
 
-//defines
-#define h_addr h_addr_list[0] /* for backward compatibility */
+#define h_addr h_addr_list[0] 
 
-#define PORT 9002 // port number
-#define MAX 1000  //maximum buffer size
+#define PORT 9002 
+#define MAX 1000 
 
-//main function
+
 int main(){
     char serverResponse[MAX];
     char clientResponse[MAX];
 
-    //creating a socket
+    
     int socketDescriptor = socket(AF_INET, SOCK_STREAM, 0);
 
-    //placeholder for the hostname and my ip address
+    
     char hostname[MAX], ipaddress[MAX];
-struct hostent *hostIP; //placeholder for the ip address
-//if the gethostname returns a name then the program will get the ip address
+struct hostent *hostIP; 
+
 if(gethostname(hostname,sizeof(hostname))==0){
-    hostIP = gethostbyname(hostname);//the netdb.h fucntion gethostbyname
+    hostIP = gethostbyname(hostname);
 }else{
 printf("ERROR:FCC4539 IP Address Not ");
 }
@@ -42,15 +38,15 @@ serverAddress.sin_addr.s_addr = INADDR_ANY;
 
 connect(socketDescriptor, (struct sockaddr *)&serverAddress, sizeof(serverAddress));
 
-// getting the address port and remote host
+
     printf("\nLocalhost: %s\n", inet_ntoa(*(struct in_addr*)hostIP->h_addr));
     printf("Local Port: %d\n", PORT);
     printf("Remote Host: %s\n", inet_ntoa(serverAddress.sin_addr));
 
     while (1)
-    {   //recieve the data from the server
+    {   
         recv(socketDescriptor, serverResponse, sizeof(serverResponse), 0);
-            //recieved data from the server successfully then printing the data obtained from the server
+            
             printf("\nSERVER : %s", serverResponse);
         
     printf("\ntext message here... :");
@@ -58,7 +54,7 @@ connect(socketDescriptor, (struct sockaddr *)&serverAddress, sizeof(serverAddres
     send(socketDescriptor, clientResponse, sizeof(clientResponse), 0);
     }
 
-    //closing the socket
+    
     close(socketDescriptor);
     return 0;
 }
