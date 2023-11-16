6b4710a8c06247b25e9bcaf244add71464688b3d
Nilesh Sharan Chaubey <105011486+Nilesh-51@users.noreply.github.com>
2023-11-16 18:53:29 +0530

    Update client.c

--- fullduplex/client.c
+++ fullduplex/client.c
@@ -1,13 +1,10 @@
 #include "stdio.h"
 #include "stdlib.h"
 #include "string.h"
-//headers for socket and related functions
 #include <sys/types.h>
 #include <sys/socket.h>
-//for including structures which will store information needed
 #include <netinet/in.h>
 #include <unistd.h>
-//for gethostbyname
 #include "netdb.h"
 #include "arpa/inet.h"
 
@@ -26,13 +23,10 @@ serverAddress.sin_family=AF_INET;
 serverAddress.sin_addr.s_addr=inet_addr("127.0.0.1");
 serverAddress.sin_port=htons(5500);
 
-/*Creating a socket, assigning IP address and port number for that socket*/
 socketDescriptor=socket(AF_INET,SOCK_STREAM,0);
 
-/*Connect establishes connection with the server using server IP address*/
 connect(socketDescriptor,(struct sockaddr*)&serverAddress,sizeof(serverAddress));
 
-/*Fork is used to create a new process*/
 cpid=fork();
 if(cpid==0)
 {
@@ -40,9 +34,7 @@ while(1)
 {
 bzero(&sendBuffer,sizeof(sendBuffer));
 printf("\nType a message here ...  ");
-/*This function is used to read from server*/
 fgets(sendBuffer,1000,stdin);
-/*Send the message to server*/
 send(socketDescriptor,sendBuffer,strlen(sendBuffer)+1,0);
 printf("\nMessage sent !\n");
 }
@@ -52,7 +44,6 @@ else
 while(1)
 {
 bzero(&recvBuffer,sizeof(recvBuffer));
-/*Receive the message from server*/
 recv(socketDescriptor,recvBuffer,sizeof(recvBuffer),0);
 printf("\nSERVER : %s\n",recvBuffer);
 }
